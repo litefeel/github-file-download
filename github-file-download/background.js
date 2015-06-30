8d2b04885a58d5dd03cdc4de6095e2775c15a4e5
lite3 <lite3@qq.com>
2015-06-30 18:07:59 +0800

    add download link for tree page

--- github-file-download/background.js
+++ github-file-download/background.js
@@ -6,9 +6,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   console.log("changeInfo.status=%s", changeInfo.status);
   if (changeInfo.status !== 'loading') return;
 
+  // https://github.com/username/reponame
+  // https://github.com/username/reponame/tree/xxxx
   // https://github.com/username/reponame/blob/xxxx
   // https://gist.github.com/username/xxxx
-  if (!tab.url.match(/https:\/\/github\.com\/[^\/]+\/[^\/]+\/blob\/.+/) &&
+  if (!tab.url.match(/https:\/\/github\.com\/[^\/]+\/[^\/]+(\/?$|\/blob\/.+|\/tree\/.+)/) &&
       !tab.url.match(/https:\/\/gist\.github\.com\/[^\/]+\/.+/)) {
     return;
   }
--- github-file-download/download-button.js
+++ github-file-download/download-button.js
@@ -1,21 +1,52 @@
+// is blod page
+function gfd_isBlob() {
+    return window.location.href.match(/https:\/\/github\.com\/[^\/]+\/[^\/]+\/blob\/.+/);
+}
+
+function gdf_insertLinkForTree() {
+    $(".octicon-file-text").not(".gfdclass").each(function(idx,elem){
+        elem = $(elem);
+        elem.addClass("gfdclass");
+        var url = elem.parent().parent().children(".content").find("a").attr("href");
+        url = url.replace("blob", "raw");
+        var filename = url.substring(url.lastIndexOf('/')+1);
+        var html = '<a href="'+url+'" download="'+filename+'" data-skip-pjax=""></a>';
+        $(html).insertBefore(elem);
+        elem.appendTo(elem.prev());
+    });
+}
+
+function gfd_insertLinkForBlob() {
+    if ($("#ext-file-download").length > 0) return;
+
+    var rawurl = $("#raw-url")
+    var url = rawurl.attr('href')
+    var filename = url.substring(url.lastIndexOf('/')+1);
+    var html = '<a href="'+url+'" id="ext-file-download" download="'+filename+'" class="btn btn-sm">Download</a>';
+    $(html).insertBefore(rawurl);
+}
+
+function gfd_insertLinkForGist() {
+    if ($("#ext-file-download").length > 0) return;
+
+    var rawurl = $(".raw-url")
+    var url = rawurl.attr('href')
+    var filename = url.substring(url.lastIndexOf('/')+1);
+    var html = '<a href="'+url+'" id="ext-file-download" download="'+filename+'" data-skip-pjax="" class="minibutton">Download</a>';
+    $(html).insertBefore(rawurl);
+}
+
 
 function gfd_onLocalChange() {
     jQuery(function ($){
-        console.log("User s has d points");
-        if ($("#ext-file-download").length > 0) return;
-
         if (window.location.hostname == "github.com") {
-            var rawurl = $("#raw-url")
-            var url = rawurl.attr('href')
-            var filename = url.substring(url.lastIndexOf('/')+1);
-            var html = '<a href="'+url+'" id="ext-file-download" download="'+filename+'" class="btn btn-sm">Download</a>';
-            $(html).insertBefore(rawurl);
+            if (gfd_isBlob()) {
+                gfd_insertLinkForBlob();
+            } else {
+                gdf_insertLinkForTree();
+            }
         } else if (window.location.hostname == "gist.github.com") {
-            var rawurl = $(".raw-url")
-            var url = rawurl.attr('href')
-            var filename = url.substring(url.lastIndexOf('/')+1);
-            var html = '<a href="'+url+'" id="ext-file-download" download="'+filename+'" data-skip-pjax="" class="minibutton">Download</a>';
-            $(html).insertBefore(rawurl);
+            gfd_insertLinkForGist();
         }
     });
 }
